9ce25ec00a54541c24bd56d55c4c49c81714a558
Xi <wangxisea@hotmail.com>
2018-03-12 20:59:48 +0100

    clean up

--- src/worldmap.js
+++ src/worldmap.js
@@ -24,6 +24,7 @@ var WorldMapcontainer = new Datamap({
 
 var colors = d3.scale.linear().domain([0, 159724601, 289724601]).range(['#ffffff', '#336699', '#112233']);
 
+//async data file loading
 // queue()
 //     .defer(d3.json, "http://localhost:63343/Cryptoanalytics/data/bitcoin_trx_nr.json")
 //     .await(ready);
@@ -40,23 +41,24 @@ var colors = d3.scale.linear().domain([0, 159724601, 289724601]).range(['#ffffff
 
 d3.json("http://localhost:63343/Cryptoanalytics/data/bitcoin_trx_nr.json", function (trx_data) {
     window.setInterval(function () {
-    WorldMapcontainer.updateChoropleth({
-        USA: colors(val_from_slider(), 'BTC', 'USA'),
-        // RUS: colors(ready(), 'BTC', 'RUS'),
-        // AUS: colors(ready(), 'BTC', 'AUS'),
-        // BRA: colors(ready(), 'BTC', 'BRA'),
-        // CAN: colors(ready(), 'BTC', 'CAN'),
-        // ZAF: colors(ready(), 'BTC', 'ZAF'),
+        WorldMapcontainer.updateChoropleth({
+            USA: colors(val_from_slider(), 'BTC', 'USA'),
+            // RUS: colors(ready(), 'BTC', 'RUS'),
+            // AUS: colors(ready(), 'BTC', 'AUS'),
+            // BRA: colors(ready(), 'BTC', 'BRA'),
+            // CAN: colors(ready(), 'BTC', 'CAN'),
+            // ZAF: colors(ready(), 'BTC', 'ZAF'),
 
-    });
+        });
     }, 500);
 
     var epoch_mul = 0;
+
     function val_from_slider() {
         if (epoch_mul == 15) {
             window.clearInterval();
         } else {
-            epoch_mul ++ ;
+            epoch_mul++;
             console.log(epoch_mul);
             var epoch = String(parseFloat(document.getElementById("slider").value) + 8640000000 * epoch_mul); //gets the oninput value
             console.log(epoch);
