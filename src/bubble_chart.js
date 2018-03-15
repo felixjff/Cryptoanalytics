5aa3c23b73aa3253ae9752899666133a62f69bdc
Hui Chen <727wuming@gmail.com>
2018-03-15 16:38:43 +0100

    Add two more hand-coded dummy data.
    The bubble chart should be able to update when the user click the slider or the map.

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -1,7 +1,7 @@
 
 
-        dataset = {
-            "date": "2018-03-08",
+         dataset1 = {
+            "country": "world",
             "children": [{"Currency":"Bitcoin","MarketCap":168241000000},
                 {"Currency":"Ethereum","MarketCap":73788400000},
                 {"Currency":"Ripple","MarketCap":34108000000},
@@ -12,12 +12,40 @@
                 {"Currency":"EOS","MarketCap":4651410000},
                 {"Currency":"Dash","MarketCap":4059470000}]
         };
+
+        dataset2 = {
+            "country": "usa",
+            "children": [{"Currency":"Bitcoin","MarketCap":16824100000},
+                {"Currency":"Ethereum","MarketCap":7378840000},
+                {"Currency":"Ripple","MarketCap":3410800000},
+                {"Currency":"Litecoin","MarketCap":1031160000},
+                {"Currency":"NEO","MarketCap":653477000},
+                {"Currency":"Stellar","MarketCap":609174000},
+                {"Currency":"Monero","MarketCap":534231000},
+                {"Currency":"EOS","MarketCap":465141000},
+                {"Currency":"Dash","MarketCap":405947000}]
+        };
+
+        dataset3 = {
+            "country": "china",
+            "children": [{"Currency":"Bitcoin","MarketCap":16424100000},
+                {"Currency":"Ethereum","MarketCap":7678840000},
+                {"Currency":"Ripple","MarketCap":3110800000},
+                {"Currency":"Litecoin","MarketCap":1931160000},
+                {"Currency":"NEO","MarketCap":613477000},
+                {"Currency":"Stellar","MarketCap":709174000},
+                {"Currency":"Monero","MarketCap":934231000},
+                {"Currency":"EOS","MarketCap":565141000},
+                {"Currency":"Dash","MarketCap":485947000}]
+        };
+
+
         // set color scheme and bubble chart size
         var diameter = 600;
         var color = d3.scaleOrdinal(d3.schemeCategory20);
 
         // set bubble parameters
-        var bubble = d3.pack(dataset)
+        var bubble = d3.pack(dataset1)
             .size([diameter - 100, diameter]) // should be equal to svg w and h
             .padding(3);
 
@@ -29,7 +57,7 @@
             .attr("class", "bubble");
 
         // set nodes size correlated with dataset.cap
-        var nodes = d3.hierarchy(dataset)
+        var nodes = d3.hierarchy(dataset1)
             .sum(function(d) { return d.MarketCap; });
 
         // append node on svg, bind the data from children class
@@ -49,6 +77,12 @@
             })
             .style("fill", function(d,i) {
                 return color(i);
+            })
+            .on("click", function(d) {
+                
+                var name = d.data.Currency;
+                return name;
+
             });
 
         node.append("text")
@@ -63,20 +97,29 @@
             })
             .attr("fill", "white");
 
-        // append market cap data as text, optional
-        //node.append("text")
-         //   .attr("dy", "1.3em")
-         //   .style("text-anchor", "middle")
-         //   .text(function(d) {
-         //       return d.data.MarketCap;
-         //   })
-         //   .attr("font-family",  "Gill Sans", "Gill Sans MT")
-         //   .attr("font-size", function(d){
-         //       return d.r/6;
-         //   })
-         //   .attr("fill", "white");
-
         d3.select(self.frameElement)
             .style("height", diameter + "px");
 
-        // add slider
+        // When the user click map, the bubble updates according to the country clicked.
+
+        var buttons = d3.select("body")
+                        .append("div")
+                        .attr("clasee", "countries-button")
+                        .selectAll("div")
+                        .data(dataset1.country)
+                        .enter()
+                        .append("button")
+                        .text(function(d){
+                            return d;
+                        });
+
+            buttons.on("click", function(d){
+                    d3.select(node)
+                        .transition()
+                        .duration(500)
+                        .style("background", "lightBlue");
+
+                    update(d);
+
+
+            })
